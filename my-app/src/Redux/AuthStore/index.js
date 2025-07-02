2aeba227bb07b669d8a11b48f9b160bd8a7889ae
Lucifer <suhasmkanter@example.com>
2025-07-02 17:54:40 +0530

    initial commit

--- my-app/src/Redux/AuthStore/index.js
+++ my-app/src/Redux/AuthStore/index.js
@@ -10,7 +10,7 @@ export const loginForm = createAsyncThunk('AuthLogin', async (formdata) => {
 
     try {
         console.log(formdata)
-        let response = await fetch('https://vtu-network.onrender.com/api/user/login', {
+        let response = await fetch('https://vtu-network12.onrender.com/api/user/login', {
             method: 'POST',
             headers: {
                 "Content-Type": 'application/json'
@@ -32,7 +32,7 @@ export const registerForm = createAsyncThunk('Authregister', async (formdata) =>
 
     try {
         console.log(formdata)
-        let response = await fetch('https://vtu-network.onrender.com/api/user/register', {
+        let response = await fetch('https://vtu-network12.onrender.com/api/user/register', {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
@@ -51,7 +51,7 @@ export const registerForm = createAsyncThunk('Authregister', async (formdata) =>
 
 export const AuthMiddleWare = createAsyncThunk('Authmiddleware', async () => {
     try {
-        let response = await fetch('https://vtu-network.onrender.com/api/user/authMiddle', {
+        let response = await fetch('https://vtu-network12.onrender.com/api/user/authMiddle', {
             credentials: 'include',
         })
         let data = await response.json()
